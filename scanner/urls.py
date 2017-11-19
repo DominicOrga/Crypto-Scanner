b3bc901daa174ae2f1c4e870a1866c75d60f9fbd
Unknown <dominicorga@gmail.com>
2017-11-20 07:49:23 +0800

    Dynamic data table using ajax

--- scanner/urls.py
+++ scanner/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.scan)
+	url(r'^$', views.scan),
+	url(r'^rescan/', views.rescan, name="rescan")
 ]
--- scanner/views.py
+++ scanner/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from libs.bittrexlib import bittrex
-import numpy as np
-
 
 def scan(request):
 	table = []
@@ -43,3 +42,45 @@ def scan(request):
 		table.append(row_data)
 
 	return render(request, "scanner/scanner.html", { "table": table })
+
+
+def rescan(request):
+	table = []
+	error = []
+
+	btx = bittrex.Bittrex(bittrex.API_KEY, bittrex.API_SECRET, api_version = bittrex.API_V2_0)
+
+	market_summaries = btx.get_market_summaries()
+
+	if not market_summaries["success"]:
+		error = ["Failed to acquire market summaries"]
+		render(request, "scanner/scanner.html", { "error": error })
+
+	markets = market_summaries["result"]
+	markets = [ms for ms in markets 
+				if ms["Market"]["BaseCurrency"] == "BTC" and 
+				ms["Summary"]["PrevDay"] >= 0.001 and 
+				ms["Summary"]["BaseVolume"] >= 750]
+
+	markets = sorted(markets, key = lambda x: x["Summary"]["BaseVolume"], reverse = True)
+
+	for ms in markets:
+		summary = ms["Summary"]
+
+		last = summary["Last"]
+		prevDay = summary["PrevDay"]
+		price_change = (last - prevDay) / prevDay 
+
+		row_data = {
+			"MarketName": summary["MarketName"],
+			"BaseVolume": "{:.3f}".format(summary["BaseVolume"]),
+			"Bid":		  "{:.8f}".format(summary["Bid"]),
+			"Ask": 		  "{:.8f}".format(summary["Ask"]),
+			"Last": 	  "{:.8f}".format(last),
+			"PrevDay": 	  "{:.8f}".format(prevDay),
+			"Change": 	  "{:.1f}%".format(price_change * 100)
+		}
+
+		table.append(row_data)
+
+	return JsonResponse({'table': table})
