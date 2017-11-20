3888ba6ab16ba9f9f14d0e5b1920382d4a8c5dee
Unknown <dominicorga@gmail.com>
2017-11-21 07:15:55 +0800

    Add market field in RsiModel

--- scanner/models.py
+++ scanner/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class RsiModel(models.Model):
+	market = models.CharField(max_length=11, default="")
 	ave_gain = models.FloatField()
 	ave_loss = models.FloatField()
 	datetime = models.DateTimeField()
@@ -13,3 +14,4 @@ class RsiModel(models.Model):
 			objects[0].delete()
 
 		super(RsiModel, self).save(*args, **kwargs)
+
