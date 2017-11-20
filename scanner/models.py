65c3730a1285aca9455da4b1e2e7196342e10035
Unknown <dominicorga@gmail.com>
2017-11-20 21:55:34 +0800

    Migrate Rsi model

--- scanner/models.py
+++ scanner/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Rsi(models.Model):
+	ave_gain = models.FloatField()
+	ave_loss = models.FloatField()
+	datetime = models.DateTimeField()
