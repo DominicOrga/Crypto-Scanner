72aa4f2561910dab0565361ece6ead720ba92023
Unknown <dominicorga@gmail.com>
2017-11-21 20:23:22 +0800

    Add RsiModel in Admin Site

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RsiModel
+
+class RsiModelAdmin(admin.ModelAdmin):
+	list_display = ("market", "ave_gain", "ave_loss", "datetime")
 
 # Register your models here.
+admin.site.register(RsiModel, RsiModelAdmin)
