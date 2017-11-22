b4a598c0a680638737145eb72ad9ef35ea6c0bcf
Unknown <dominicorga@gmail.com>
2017-11-22 22:50:34 +0800

    Add navbar

--- scanner/urls.py
+++ scanner/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.load_scanner),
+	url(r'^$', views.load_scanner, name="scanner"),
 	url(r'^rescan/', views.scan, name="scan")
 ]
