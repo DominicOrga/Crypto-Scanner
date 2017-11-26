714ae24dc9d1f4150d172b1e27d5ab1627f63c22
Unknown <dominicorga@gmail.com>
2017-11-26 13:20:40 +0800

    Create strata.html and set url and view to load it

--- scanner/models.py
+++ scanner/models.py
@@ -46,7 +46,7 @@ class MarketGroupModel(models.Model):
 	markets = models.ManyToManyField(MarketModel)
 
 	def __str__(self):
-		return str(self.id) + " " + " ".join(m.market[4:] for m in self.markets.all())
+		return " ".join(m.market[4:] for m in self.markets.all())
 
 	def save(self, *args, **kwargs):
 		super(MarketGroupModel, self).save(*args, **kwargs)
