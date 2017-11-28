b86d142d4cce06d86fa9dc9dccf42b9344059171
Unknown <dominicorga@gmail.com>
2017-11-28 18:56:54 +0800

    Filter recent markets in  strata/views.strata_markets
    Very long list comprehension...

--- strata/views.py
+++ strata/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from scanner.models import MarketModel
-# Create your views here.
+import datetime
+
 def load_strata(request):
 	return render(request, "strata/strata.html")
 
 def strata_markets(request):
 
 	try:
-		markets = MarketModel.objects.filter(rsi__lte = 25, change_24h__gte = 0.05, change_12h__gte = 0.025, change_6h__gte = 0)
-		markets_dict = [model_to_dict(m) for m in markets]
+		# expected: rsi__lte = 25. rsi__lte = 100 is used for debugging
+		markets = MarketModel.objects.filter(rsi__lte = 100, change_24h__gte = 0.05, change_12h__gte = 0.025, change_6h__gte = 0)
+		dt_now = datetime.datetime.utcnow()
+		timedelta = datetime.timedelta(minutes=15)
+
+		markets_dict = [dict(model_to_dict(m), is_recent = (timedelta - (dt_now - m.datetime_created.replace(tzinfo=None))).days >= 0) for m in markets]
 
 		return JsonResponse({ "markets": markets_dict })
 
 	except MarketModel.DoesNotExist:
-		return JsonResponse({ "markets": "" })
+		return None
 		
